b2bcf5426b17c6f19c0dbb14af548a37bd096011
SBfin <silviobusonero@gmail.com>
2025-07-05 18:12:31 +0200

    ronin

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,23 +1,27 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
-} from 'wagmi/chains';
+import { defineChain } from 'viem';
+
+export const ronin = defineChain({
+  id: 2020,
+  name: 'Ronin',
+  nativeCurrency: { name: 'RON', symbol: 'RON', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['https://api.roninchain.com/rpc'] },
+  },
+  blockExplorers: {
+    default: { name: 'Ronin Explorer', url: 'https://app.roninchain.com' },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 26020753,
+    },
+  },
+});
 
 export const config = getDefaultConfig({
   appName: 'RainbowKit App',
   projectId: 'YOUR_PROJECT_ID',
-  chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
-  ],
+  chains: [ronin],
   ssr: true,
 });
